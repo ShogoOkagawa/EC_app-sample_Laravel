7b0c560388043fbad5d030e0759f23ff2df84fed
Shogo Okagawa <okagawa1981@gmail.com>
2021-12-01 06:54:50 +0900

    createpaginate

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,14 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+
+
 <div class="row">
-    <div class="col-9">
+    <div class="col-2">
+      @component('components.sidebar', ['categories' => $categories, 'major_category_names' => $major_category_names])
+      @endcomponent
+   </div>
+   <div class="col-9">
+       <div class="container">
+           @if ($category !== null)
+            <a href="/">トップ</a> > <a href="#">{{ $category->major_category_name }}</a> > {{ $category->name }}
+            <h1>{{ $category->name }}の商品一覧{{ $total_count }}件</h1>
+           @endif
+       </div>
         <div class="container mt-4">
             <div class="row w-100">
                 @foreach($products as $product)
                 <div class="col-3">
                     <a href="{{route('products.show', $product)}}">
                         <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
+                        {{ $product->name }}
                     </a>
                     <div class="row">
                         <div class="col-12">
@@ -22,6 +35,10 @@
                 @endforeach
             </div>
         </div>
+
+        {{ $products->appends(request()->query())->links() }}
+
+        <a href="{{route('products.create')}}" class="btn btn-primary m-4">New Product</a>
     </div>
 </div>
 @endsection
